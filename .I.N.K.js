40a2ae6ea5211bd9e0e63707ad0e2ec86bc84edc
Flnah <157045011+Flnah@users.noreply.github.com>
2024-01-18 12:33:29 -0400

    Add files via upload

---
+++ .I.N.K.js
@@ -0,0 +1,20 @@
+const righturl = (window.location.href);
+    if (window.location.href.includes("/block_screen.html?id=")){
+        if (window.location.href.includes("https://localhost:")){
+            const righturl = (true)
+        }    
+    } else {
+        const righturl = (false)};
+
+        //Delete Alerts In Future Development
+        if (righturl) { 
+            localStorage.setItem('Inked','f')
+            alert("TEST SUCCESSFUL")
+            console.log("Starting")} else {alert("TEST SUCCESSFUL")
+            console.log("Wrong Site")};
+        if (righturl){
+            if (confirm('ᴉɟᴉM ǝlqɐsᴉp o┴ ʇuɐM no⅄ ǝɹnS no⅄ ǝɹ∀')){
+            //Wifi Disabling Code
+            }}
+
+        
